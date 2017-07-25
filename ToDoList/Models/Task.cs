bbbb0d42452638d3e32d57ff47caad56f7ed4c86
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-25 15:12:05 -0400

    changes to lists and tasks

---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskID { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+
+        [Display(Name = "Task")]
+        public string ListID { get; set; }
+        //Navigation Property
+        public virtual ICollection<List> Lists { get; set; }
+    
+    }
+}
